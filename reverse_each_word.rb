ca08f14a4f66559484d7063d857dac88df900a9d
Justin Langlinais <justinclanglinais@gmail.com>
2020-04-04 20:09:26 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,8 +3,7 @@ def reverse_each_word(string)
   array = string.split ()
   array.count do |i|
     word_array = i.split ()
-    flipped_word = word_array.reverse_each ()
-    flipped_word << final_array
+    puts word_array
   end
   puts final_array
 end 
